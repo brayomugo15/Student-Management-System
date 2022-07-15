58419e7381d73aeb722dbdf747c9dc3d2702f700
Brian Mugo <brayomugo2015@gmail.com>
2022-07-15 11:12:32 +0300

    - Add Student entity

--- src/main/java/entity/Student.java
+++ src/main/java/entity/Student.java
@@ -1,4 +1,48 @@
 package entity;
 
 public class Student {
+
+    private long id;
+    private String fname;
+    private String lname;
+    private String email;
+
+    public Student(long id, String fname, String lname, String email) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
